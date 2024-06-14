464968b1c78d70bc938058922ca7c0599d6b6dea
fateshelled <53618876+fateshelled@users.noreply.github.com>
2024-06-15 00:16:54 +0900

    [Update] Support m, s model

--- examples/export_onnx.py
+++ examples/export_onnx.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from pathlib import Path
 
 import hydra
@@ -14,7 +15,7 @@ from torch import nn
 
 from yolo.config.config import Config
 from yolo.model.yolo import create_model
-from yolo.utils.bounding_box_utils import AnchorBoxConverter
+from yolo.utils.bounding_box_utils import Vec2Box
 from yolo.utils.logging_utils import custom_logger
 
 
@@ -22,27 +23,27 @@ class YOLOv9WithPostprocess(nn.Module):
     def __init__(self, cfg: Config):
         super(YOLOv9WithPostprocess, self).__init__()
         self.device = torch.device(cfg.device)
-        self.model = create_model(cfg).to(self.device)
-        self.anchor2box = AnchorBoxConverter(cfg.model, cfg.image_size, self.device)
+        self.model = create_model(cfg.model, cfg.weight).to(self.device)
+        self.vec2box = Vec2Box(self.model, cfg.image_size, self.device)
 
     def forward(self, tensor: torch.Tensor):
         raw_output = self.model(tensor)
-        # predict, _ = self.anchor2box(raw_output[0][3:], with_logits=True)
-        preds_cls, preds_box, _ = self.anchor2box(raw_output[0][3:], with_logits=True)
+        preds_cls, pred_anc, preds_box = self.vec2box(raw_output["Main"])
         return preds_cls, preds_box
 
 
-@hydra.main(config_path="../yolo/config", config_name="config", version_base=None)
+@hydra.main(config_path="../yolo/config", config_name="config_m", version_base=None)
 def main(cfg: Config):
     custom_logger()
+
+    model_post = YOLOv9WithPostprocess(cfg)
 
     device = torch.device(cfg.device)
     input_shape = (1, 3, cfg.image_size[0], cfg.image_size[1])
     dummy_input = torch.randn(input_shape).to(device)
 
-    model_post = YOLOv9WithPostprocess(cfg)
-    model_name = "yolov9mit_with_post.onnx"
-    simp_model_name = "yolov9mit_with_post.sim.onnx"
+    model_name = f"{os.path.splitext(os.path.basename(cfg.weight))[0]}.vec2box.onnx"
+    simp_model_name = os.path.splitext(model_name)[0] + ".sim.onnx"
     opset_version = 12
     torch.onnx.export(
         model_post,
